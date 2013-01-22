23ec72be69be763513f2aa8d02771424c62f9368
Felix Mossberg <hakurou46@gmail.com>
2013-01-22 17:37:42 +0100

    Inventory implementation

--- WinterDreams/DamageHitBox.h
+++ WinterDreams/DamageHitBox.h
@@ -6,13 +6,26 @@
 
 class DamageHitBox:public PlayerRelated{
 public:
+    enum DamageTypes{NORMAL, ANNAT};
+    /*
+    //  Needs a numeric value for the damage, a size and a type of damage.
+    //  Damage types are defined in DamageHitBox.h
+    */
 	DamageHitBox(unsigned int damage, const sf::Rect<float>& hitBox, DamageTypes type);
 	~DamageHitBox();
+    /*
+    //  Returns the rectal hitbox
+    */
 	const sf::Rect<float>& getHitBox() const;
+    /*
+    //  The death of a hitbox creeps closer
+    */
 	void update(int milliseconds);
+    /*
+    //  Draws the hitbox (for debugging reasons)
+    */
 	void drawSelf();
 private:
-	enum DamageTypes{NORMAL, ANNAT};
 	unsigned int mDamage;
 	sf::Rect<float> mHitBox;
 	int mMilliseconds;
--- WinterDreams/Inventory.h
+++ WinterDreams/Inventory.h
@@ -6,11 +6,18 @@
 
 class Inventory{
 public:
-	//Returns the number of items you possess
+	/*
+    //  Returns the number of items you possess.
+    */
 	unsigned short hasItem(string item);
-	//Adds number of an item
+	/*
+    //  Adds a number of item to the inventory.
+    */
 	void giveItem(string item, unsigned short number);
-	//Removes number of an item
+    /*
+	//  Removes a number of item to the inventory.
+    //  If the item hasn't been mapped, does nothing.
+    */
 	void takeItem(string item, unsigned short number);
 private:
 	map<std::string, unsigned short> mInventoryMap;
